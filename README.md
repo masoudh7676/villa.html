2f7350320e9ea2c81496e8278ae8e9092bc14e3f
Masoud haseli <haselimasoud@gmail.com>
2024-07-24 18:07:31 +0330

    continue work

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## this Villa landing page created by: html, css & js and contain page's: Home, Properties, property Details, Contact us, and i did not use bootstrap or css tailwind.
+
+## Some Features:
+### 1. for better user experience i added left and right arrow for next or previous image.
+### 2. has customized scrollbar.
+
+
+ ![image](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+ ![image](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+ ![image](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
---
+++ script/app.js
@@ -0,0 +1,102 @@
+let $ = document
+const slider = $.querySelector('.slider');
+const slides = $.querySelectorAll('.slide');
+let currentSlide = 0;
+const prevButton = $.querySelector('.prev');
+const nextButton = $.querySelector('.next');
+function moveSlide(direction) {       //Image Slider
+  const newSlide = (currentSlide + direction + slides.length) % slides.length;
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - newSlide) * 100}%)`;
+  });
+  currentSlide = newSlide;
+}
+prevButton.addEventListener('click', () => {    //Previous Image
+  moveSlide(-1);
+});
+nextButton.addEventListener('click', () => {    //Next Image
+  moveSlide(1);
+});
+$.addEventListener ('keydown', (e) =>{    //Left And Right Arrow Key Function For Image Slider
+    if (e.key === 'ArrowLeft') {
+        moveSlide(-1)
+    } else if (e.key === 'ArrowRight'){
+        moveSlide (1)
+    }
+})
+$.addEventListener('DOMContentLoaded', () => {  //Onload Event
+    const preloadOverlay = document.querySelector('.preload-overlay');
+    preloadOverlay.style.display = 'none';
+});
+const hamMenu = $.querySelector ('.burger__menu')
+var mobileMenu = $.querySelector ('.mobile__menu')
+hamMenu.addEventListener('click', () => {     //Hamburger Menu Function (Toggle Buttons)
+  hamMenu.classList.toggle('active')
+  mobileMenu.classList.toggle('active')
+})
+$.addEventListener('keydown', (e) =>{       //When Press ESC button The Ham Menu Will Closed
+  if (e.keyCode == 27){
+    hamMenu.classList.remove('active')
+    mobileMenu.classList.remove('active')
+  }
+})
+document.addEventListener('click', (e) => { // Closing Mobile Menu When clicking anywhere except the menu
+  if (!mobileMenu.contains(e.target) && !e.target.classList.contains('burger__menu')) {
+    hamMenu.classList.remove('active');
+    mobileMenu.classList.remove('active');
+  }
+});
+window.addEventListener('resize', () => {   //When width Exceeds 730 the Hame Menu Will Disappeared
+  if (window.innerWidth > 730) {
+    hamMenu.classList.remove('active');
+    mobileMenu.classList.remove('active');
+  }
+});
+const titles = $.querySelectorAll('.c__title');
+titles.forEach((title, index) =>{
+  title.addEventListener('click', () =>{
+    // Remove the 'how' class and add the 'hide' class for all other content elements
+    titles.forEach((otherTitle, otherIndex) =>{
+      if (otherTitle!== title) { // exclude the current title
+        const otherContent = otherTitle.nextElementSibling;
+        otherContent.classList.remove('show');
+        otherContent.classList.add('hide');
+        otherTitle.style.color = 'black';
+      }
+    });
+
+    // Toggle the 'how' class and update the color for the clicked title and its content
+    const content = title.nextElementSibling;
+    content.classList.toggle('show');
+    content.classList.toggle('hide'); // toggle both classes
+    title.style.color = content.classList.contains('show')? '#f35525' : 'black';
+  });
+});
+
+const buttons = $.querySelectorAll('.active__btn, .villa__btn, .penthouse__btn');
+const locations = $.querySelectorAll('.main__items, .main__items--villa, .main__items--penthouse');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // Show the corresponding content element
+    locations.forEach((location, locationIndex) => {
+      if (locationIndex === index && window.innerWidth >= 1120) {
+        location.style.setProperty('display', 'grid', 'important');
+      } else if (locationIndex === index && window.innerWidth <= 1120) {
+        location.style.setProperty('display', 'flex', 'important');
+        location.style.flexDirection = 'column';
+        location.style.alignItems = 'center';
+      } else {
+        location.style.setProperty('display', 'none', 'important');
+      }
+    });
+          // Update button colors
+          buttons.forEach((otherButton) => {
+            if (otherButton !== button) {
+              otherButton.style.backgroundColor = 'black'; // reset to default color
+            } else {
+              button.style.backgroundColor = '#f35525'; // set to orange
+            }
+          });
+  });
+});
