fe65972bf17053c4a311bc9dda7f5696708619e8
Masoud Haseli <haselimasoud@gmail.com>
2024-08-11 19:55:01 +0330

    added plugin folder 4 isotope.js and moved jquery from html file to plugin folder

---
+++ script/plugins/isotope.js
@@ -0,0 +1,27 @@
+                // Get the container element
+                var $container = $('.properties__contents');
+              
+                // Initialize Isotope
+                $container.isotope({
+                  itemSelector: '.content',
+                  layoutMode: 'fitRows',
+                  responsive: [
+                    {
+                        breakpoint: 442,
+                        options: {
+                            layoutMode: 'vertical',
+                        },
+                    },
+                  ]
+                });
+              
+                // Filter buttons
+                $('.right__buttons .button').on('click', function() {
+                    $('.right__buttons .button').removeClass('active__btn');
+                    $(this).addClass('active__btn');
+                  var filterValue = $(this).attr('data-filter');
+                  $container.isotope({ filter: filterValue });
+                });
+              
+                // Show all items on page load
+                $('.right__buttons .active__btn').trigger('click');
